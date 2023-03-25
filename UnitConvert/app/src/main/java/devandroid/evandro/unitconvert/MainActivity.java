e9f5fad78bc0e00762daa6d5014ff768578f0369
Evandro <evancastro31@hotmail.com>
2023-03-25 11:40:53 -0300

    Aula 75

--- UnitConvert/app/src/main/java/devandroid/evandro/unitconvert/MainActivity.java
+++ UnitConvert/app/src/main/java/devandroid/evandro/unitconvert/MainActivity.java
@@ -3,12 +3,33 @@ package devandroid.evandro.unitconvert;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView welcome_text,result_text;
+    Button btn;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        welcome_text = findViewById(R.id.welcome_textView);
+        result_text=  findViewById(R.id.result_textView);
+        btn=  findViewById(R.id.btn);
+        editText = findViewById(R.id.editText);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
     }
 }
