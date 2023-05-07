f5cc498b36143490ef122a95bc8a2b7e5c4dc98d
Evandro <evancastro31@hotmail.com>
2023-05-07 18:36:58 -0300

    cadastro de usuario no firebase

---
+++ cityfood/app/src/main/java/devandroid/evandro/cityfood/activity/usuario/UsuarioHomeActivity.java
@@ -0,0 +1,16 @@
+package devandroid.evandro.cityfood.activity.usuario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import devandroid.evandro.cityfood.R;
+
+public class UsuarioHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario_home);
+    }
+}
