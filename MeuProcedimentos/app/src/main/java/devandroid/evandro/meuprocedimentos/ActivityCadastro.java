7c3a7045386a73be79c7d405fd648d4508709cb7
Evandro <evancastro31@hotmail.com>
2023-05-11 20:14:04 -0300

    teste

--- MeuProcedimentos/app/src/main/java/devandroid/evandro/meuprocedimentos/ActivityCadastro.java
+++ MeuProcedimentos/app/src/main/java/devandroid/evandro/meuprocedimentos/ActivityCadastro.java
@@ -2,6 +2,7 @@ package devandroid.evandro.meuprocedimentos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class ActivityCadastro extends AppCompatActivity {
@@ -10,5 +11,11 @@ public class ActivityCadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+        conectarTelaXmlComJava();
+    }
+    private void conectarTelaXmlComJava(){
+        findViewById(R.id.btn_buscar_paciente).setOnClickListener(v ->
+                startActivity(new Intent(this, CadastroUsuarioActivity.class)));
+
     }
 }
