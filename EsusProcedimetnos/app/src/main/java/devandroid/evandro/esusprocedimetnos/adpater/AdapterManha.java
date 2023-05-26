9c3c77a660fe55e2d917fac8bec3843dcb06ae98
Evandro <evancastro31@hotmail.com>
2023-05-26 20:22:58 -0300

    criando os fragments e layout recyclerview

--- EsusProcedimetnos/app/src/main/java/devandroid/evandro/esusprocedimetnos/adpater/AdapterManha.java
+++ EsusProcedimetnos/app/src/main/java/devandroid/evandro/esusprocedimetnos/adpater/AdapterManha.java
@@ -1,4 +1,22 @@
 package devandroid.evandro.esusprocedimetnos.adpater;
 
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import devandroid.evandro.esusprocedimetnos.model.Consulta;
+
 public class AdapterManha {
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
