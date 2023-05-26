60a0ee84cf9d6a5f0ec6d4c8f260f9230038fb8d
Evandro <evancastro31@hotmail.com>
2023-05-26 16:56:09 -0300

    criando pacotes

--- EsusProcedimetnos/app/src/main/java/devandroid/evandro/esusprocedimetnos/view/MainActivity.java
+++ EsusProcedimetnos/app/src/main/java/devandroid/evandro/esusprocedimetnos/view/MainActivity.java
@@ -1,8 +1,10 @@
-package devandroid.evandro.esusprocedimetnos;
+package devandroid.evandro.esusprocedimetnos.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import devandroid.evandro.esusprocedimetnos.R;
 
 public class MainActivity extends AppCompatActivity {
 
