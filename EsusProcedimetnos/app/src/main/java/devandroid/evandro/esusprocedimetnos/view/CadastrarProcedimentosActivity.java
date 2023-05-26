c6616caf5a765798d30dfea6ea510fa5b74d5b0e
Evandro <evancastro31@hotmail.com>
2023-05-26 17:01:02 -0300

    criando telas

---
+++ EsusProcedimetnos/app/src/main/java/devandroid/evandro/esusprocedimetnos/view/CadastrarProcedimentosActivity.java
@@ -0,0 +1,16 @@
+package devandroid.evandro.esusprocedimetnos.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import devandroid.evandro.esusprocedimetnos.R;
+
+public class CadastrarProcedimentosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_procedimentos);
+    }
+}
---
+++ EsusProcedimetnos/app/src/main/java/devandroid/evandro/esusprocedimetnos/view/ListarProcedimentosActivity.java
@@ -0,0 +1,16 @@
+package devandroid.evandro.esusprocedimetnos.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import devandroid.evandro.esusprocedimetnos.R;
+
+public class ListarProcedimentosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_procedimentos);
+    }
+}
