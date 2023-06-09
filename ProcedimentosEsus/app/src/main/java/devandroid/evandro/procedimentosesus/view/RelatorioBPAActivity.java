d2f66efe274ebe6283ec1b0fc6a9fd60c71b3784
Evandro <evancastro31@hotmail.com>
2023-06-09 10:06:11 -0300

    projeto base finalizado

--- ProcedimentosEsus/app/src/main/java/devandroid/evandro/procedimentosesus/view/RelatorioBPAActivity.java
+++ ProcedimentosEsus/app/src/main/java/devandroid/evandro/procedimentosesus/view/RelatorioBPAActivity.java
@@ -1,5 +1,8 @@
 package devandroid.evandro.procedimentosesus.view;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.EditText;
@@ -7,10 +10,12 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import devandroid.evandro.procedimentosesus.R;
@@ -22,34 +27,45 @@ import devandroid.evandro.procedimentosesus.model.Paciente;
 
 public class RelatorioBPAActivity extends AppCompatActivity {
 
+
     private RecyclerView rv_bpa;
-    private EditText ed_data_final, ed_data_inicial;
-    private ImageView iv_filtrar;
+    private static EditText ed_data_final, ed_data_inicial;
+    private ImageView iv_filtrar, iv_data_inicial, iv_data_final;
 
     private List<Paciente> consultaList;
     PacienteController pacienteController;
     BpaAdapter bpaAdapter;
-
-    String dataI, dataf, sDataI, sDataf;
     String dataInicial;
     String dataFinal;
+    Boolean selecionou = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relatorio_bpaactivity);
 
+        Calendar calendar = Calendar.getInstance();
+
         pacienteController = new PacienteController(this);
+
         iniciaComponente();
 
 
+        iv_data_inicial.setOnClickListener(view -> {
+            DialogFragment dialogFragment = new DatePicker();
+            dialogFragment.show(getSupportFragmentManager(),"DataInicial");
+        });
+        iv_data_final.setOnClickListener(view -> {
+            DialogFragment dialogFragment = new DatePicker();
+            dialogFragment.show(getSupportFragmentManager(),"DataFinal");
+        });
+
         iv_filtrar.setOnClickListener(view -> {
 
+
             if (!TextUtils.isEmpty(ed_data_inicial.getText().toString()) && !TextUtils.isEmpty(ed_data_final.getText().toString())) {
-                dataI = ed_data_inicial.getText().toString();
-                dataf = ed_data_final.getText().toString();
-                dataInicial = AppUtil.getDataAtualFormatoAmericanoParaDB(dataI.replaceAll("/", "-"));
-                dataFinal = AppUtil.getDataAtualFormatoAmericanoParaDB(dataf.replaceAll("/", "-"));
+                dataInicial = AppUtil.getDataAtualFormatoAmericanoParaDB(ed_data_inicial.getText().toString());
+                dataFinal = AppUtil.getDataAtualFormatoAmericanoParaDB(ed_data_final.getText().toString());
                 configRecyclerView();
             } else {
                 Toast.makeText(this, "Selecione um intervalo de datas validos", Toast.LENGTH_LONG).show();
@@ -58,11 +74,14 @@ public class RelatorioBPAActivity extends AppCompatActivity {
 
     }
 
+
     private void iniciaComponente() {
-        rv_bpa=findViewById(R.id.rv_bpa);
-        ed_data_final =findViewById(R.id.ed_data_final);
-        ed_data_inicial =findViewById(R.id.ed_data_inicial);
-        iv_filtrar =findViewById(R.id.iv_filtrar);
+        rv_bpa = findViewById(R.id.rv_bpa);
+        ed_data_final = findViewById(R.id.ed_data_final);
+        ed_data_inicial = findViewById(R.id.ed_data_inicial);
+        iv_data_final = findViewById(R.id.iv_data_final);
+        iv_data_inicial = findViewById(R.id.iv_data_inicial);
+        iv_filtrar = findViewById(R.id.iv_filtrar);
     }
 
     private void configRecyclerView() {
@@ -93,10 +112,64 @@ public class RelatorioBPAActivity extends AppCompatActivity {
         }
         for (int i = 0; i < cpf.size(); i++) {
 
-            consultas.add(pacienteController.getTodasDataCurativoPorCpf(cpf.get(i),dataInicial,dataFinal));
+            consultas.add(pacienteController.getTodasDataCurativoPorCpf(cpf.get(i), dataInicial, dataFinal));
         }
 
         return consultas;
+    }
+
+    public static class DatePicker extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            Calendar calendar = Calendar.getInstance();
+            int dia = calendar.get(Calendar.DAY_OF_MONTH);
+            int mes = calendar.get(Calendar.MONTH);
+            int ano = calendar.get(Calendar.YEAR);
+
+            DatePickerDialog DatePickerTradicional = new DatePickerDialog(getActivity(), AlertDialog.THEME_TRADITIONAL, this, ano, mes, dia);
+            DatePickerDialog DatePickerHoloLight = new DatePickerDialog(getActivity(), AlertDialog.THEME_HOLO_LIGHT, this, ano, mes, dia);
+            DatePickerDialog DatePickerHoldDark = new DatePickerDialog(getActivity(), AlertDialog.THEME_HOLO_DARK, this, ano, mes, dia);
+            DatePickerDialog DatePickerDefaultLight = new DatePickerDialog(getActivity(), AlertDialog.THEME_DEVICE_DEFAULT_LIGHT, this, ano, mes, dia);
+            DatePickerDialog DatePickerDefaultDark = new DatePickerDialog(getActivity(), AlertDialog.THEME_DEVICE_DEFAULT_DARK, this, ano, mes, dia);
+            return DatePickerHoldDark;
+        }
+
+        @Override
+        public void onDateSet(android.widget.DatePicker datePicker, int iAno, int iMes, int iDia) {
+
+            String dia, mes, ano;
+
+            dia = String.valueOf(iDia);
+            mes = String.valueOf(((iMes) + 1));
+            ano = String.valueOf(iAno);
+
+
+            dia = (Calendar.DAY_OF_MONTH < 10) ? "0" + dia : dia;
+
+            dia = (dia.length() > 2) ? dia.substring(1, 3) : dia;
+
+            int mesAtual = (Calendar.MONTH) + 1;
+
+            mes = (mesAtual <= 9) ? "0" + mes : mes;
+
+            mes = (mes.length() > 2) ? mes.substring(1, 3) : mes;
+
+
+            String  dataAtual = dia + "-" + mes + "-" + ano;
+
+            String tagClicada= getTag();
+            if(tagClicada.equals("DataInicial")){
+
+                ed_data_inicial.setText(dataAtual);
+
+            }else if(tagClicada.equals("DataFinal")){
+                ed_data_final.setText(dataAtual);
+            }
+
+        }
     }
 
 
--- ProcedimentosEsus/app/src/main/java/devandroid/evandro/procedimentosesus/view/RelatorioPabActivity.java
+++ ProcedimentosEsus/app/src/main/java/devandroid/evandro/procedimentosesus/view/RelatorioPabActivity.java
@@ -1,15 +1,19 @@
 package devandroid.evandro.procedimentosesus.view;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
-import java.text.ParseException;
+import java.util.Calendar;
 
 import devandroid.evandro.procedimentosesus.R;
 import devandroid.evandro.procedimentosesus.api.AppUtil;
@@ -17,11 +21,14 @@ import devandroid.evandro.procedimentosesus.controller.PacienteController;
 
 public class RelatorioPabActivity extends AppCompatActivity {
 
-    private EditText ed_data_inicial, ed_data_final;
+    private static EditText ed_data_inicial_pab, ed_data_final_pab;
     private ImageView iv_filtrar;
+    private  ImageView iv_data_inicial_pab ;
+    private ImageView iv_data_final_pab;
     private TextView tv_total_pa, tv_total_glicemia, tv_covid, tv_total_visita, tv_total_Nebulizacao, tv_total_r_pontos, tv_HIV, tv_hep_c, tv_hep_b, tv_sifilis, tv_dengue;
-    String dataI,dataf,sDataI,sDataf;
+
     String dataInicial;
+
     String dataFinal;
     private PacienteController pacienteController;
 
@@ -34,25 +41,29 @@ public class RelatorioPabActivity extends AppCompatActivity {
 
         pacienteController = new PacienteController(this);
         iniciaComponentes();
+
+
+
+        iv_data_inicial_pab.setOnClickListener(view -> {
+            DialogFragment dialogFragment = new RelatorioPabActivity.DatePicker();
+            dialogFragment.show(getSupportFragmentManager(),"DataInicial");
+
+        });
+        iv_data_final_pab.setOnClickListener(view -> {
+            DialogFragment dialogFragment = new RelatorioPabActivity.DatePicker();
+            dialogFragment.show(getSupportFragmentManager(),"DataFinal");
+        });
+
+
         iv_filtrar.setOnClickListener(view -> {
 
+            if (!TextUtils.isEmpty(ed_data_inicial_pab.getText().toString()) && !TextUtils.isEmpty(ed_data_final_pab.getText().toString())) {
 
-            dataI = ed_data_inicial.getText().toString();
-            dataf = ed_data_final.getText().toString();
-
-
-            if(!dataI.isEmpty() && !dataf.isEmpty()) {
-
-                    dataI = ed_data_inicial.getText().toString().trim();
-                    dataf = ed_data_final.getText().toString().trim();
-                    sDataI =dataI.replaceAll("/", "-");
-                    sDataf =dataf.replaceAll("/", "-");
-                    dataInicial = AppUtil.getDataAtualFormatoAmericanoParaDB(sDataI);
-                    dataFinal = AppUtil.getDataAtualFormatoAmericanoParaDB(sDataf);
-                    totalMensalPab(dataInicial, dataFinal);
-
-            }else{
-                Toast.makeText(this,"Selecione um intervalo de datas validos",Toast.LENGTH_LONG).show();
+                dataInicial = AppUtil.getDataAtualFormatoAmericanoParaDB(ed_data_inicial_pab.getText().toString());
+                dataFinal = AppUtil.getDataAtualFormatoAmericanoParaDB(ed_data_final_pab.getText().toString());
+                totalMensalPab(dataInicial,dataFinal);
+            } else {
+                Toast.makeText(this, "Selecione um intervalo de datas validos", Toast.LENGTH_LONG).show();
             }
 
         });
@@ -60,10 +71,12 @@ public class RelatorioPabActivity extends AppCompatActivity {
     }
 
     private void iniciaComponentes() {
-        ed_data_inicial = findViewById(R.id.ed_data_inicial);
-        ed_data_final = findViewById(R.id.ed_data_final);
+        ed_data_inicial_pab = findViewById(R.id.ed_data_inicial_pab);
+        ed_data_final_pab = findViewById(R.id.ed_data_final_pab);
+        iv_data_final_pab = findViewById(R.id.iv_data_final_pab);
+        iv_data_inicial_pab = findViewById(R.id.iv_data_inicial_pab);
 
-        iv_filtrar = findViewById(R.id.iv_filtrar);
+        iv_filtrar = findViewById(R.id.iv_filtrar_pab);
 
         tv_total_pa = findViewById(R.id.tv_total_pa);
         tv_total_glicemia = findViewById(R.id.tv_total_glicemia);
@@ -91,5 +104,59 @@ public class RelatorioPabActivity extends AppCompatActivity {
         tv_sifilis.setText(String.valueOf(pacienteController.listTotalProcedimentosPab(dataInicial, dataFinal, AppUtil.SIFILIS)));
         tv_dengue.setText(String.valueOf(pacienteController.listTotalProcedimentosPab(dataInicial, dataFinal, AppUtil.DENGUE)));
         tv_covid.setText(String.valueOf(pacienteController.listTotalProcedimentosPab(dataInicial, dataFinal, AppUtil.COVID)));
+    }
+    public static class DatePicker extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            Calendar calendar = Calendar.getInstance();
+            int dia = calendar.get(Calendar.DAY_OF_MONTH);
+            int mes = calendar.get(Calendar.MONTH);
+            int ano = calendar.get(Calendar.YEAR);
+
+            DatePickerDialog DatePickerTradicional = new DatePickerDialog(getActivity(), AlertDialog.THEME_TRADITIONAL, this, ano, mes, dia);
+            DatePickerDialog DatePickerHoloLight = new DatePickerDialog(getActivity(), AlertDialog.THEME_HOLO_LIGHT, this, ano, mes, dia);
+            DatePickerDialog DatePickerHoldDark = new DatePickerDialog(getActivity(), AlertDialog.THEME_HOLO_DARK, this, ano, mes, dia);
+            DatePickerDialog DatePickerDefaultLight = new DatePickerDialog(getActivity(), AlertDialog.THEME_DEVICE_DEFAULT_LIGHT, this, ano, mes, dia);
+            DatePickerDialog DatePickerDefaultDark = new DatePickerDialog(getActivity(), AlertDialog.THEME_DEVICE_DEFAULT_DARK, this, ano, mes, dia);
+            return DatePickerHoldDark;
+        }
+
+        @Override
+        public void onDateSet(android.widget.DatePicker datePicker, int iAno, int iMes, int iDia) {
+
+            String dia, mes, ano;
+
+            dia = String.valueOf(iDia);
+            mes = String.valueOf(((iMes) + 1));
+            ano = String.valueOf(iAno);
+
+
+            dia = (Calendar.DAY_OF_MONTH < 10) ? "0" + dia : dia;
+
+            dia = (dia.length() > 2) ? dia.substring(1, 3) : dia;
+
+            int mesAtual = (Calendar.MONTH) + 1;
+
+            mes = (mesAtual <= 9) ? "0" + mes : mes;
+
+            mes = (mes.length() > 2) ? mes.substring(1, 3) : mes;
+
+
+            String  dataAtual = dia + "-" + mes + "-" + ano;
+
+            String tagClicada= getTag();
+
+            if(tagClicada.equals("DataInicial")){
+
+                ed_data_inicial_pab.setText(dataAtual);
+
+            }else if(tagClicada.equals("DataFinal")){
+                ed_data_final_pab.setText(dataAtual);
+            }
+
+        }
     }
 }
